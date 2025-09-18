46c14ca547ac16f28e4127ea3d27189521c3186e
Elias Cuevas Herrera <e.cuevas24@info.uas.edu.mx>
2025-09-18 11:10:21 -0700

    Create elias.rs
    este es un trabajo

---
+++ elias.rs
@@ -0,0 +1,22 @@
+fn main(){
+
+    println!("Hola Mundo");
+    let num1=10;
+    let num2=10;
+
+    let sumar = num1 + num2;
+    println!("sumar:{}",sumar);
+   
+    let restar= num1 -  num2;
+    println!("restar:{}",restar);
+
+    let dividir = num1 / num2;
+    println!("dividir:{}",dividir);
+
+    let multiplicar = num1 * num2;
+    println!("Multiplicar:{}", multiplicar);
+
+    let modulo = num1 % num2;
+    println!("modulo:{}", modulo);
+
+}
