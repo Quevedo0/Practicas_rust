d7598afe21f2c307dab216d16624e60d3be3e329
Felipe <ft69120@gmail.com>
2025-09-18 10:42:29 -0700

    prom.rs

---
+++ prom.rs
@@ -0,0 +1,17 @@
+fn main(){
+let x = 5;
+let y = 10;
+
+
+println!("la suma de {} y {} es {}", x, y, x + y);
+
+println!("la resta de {} y {} es {}", x, y, x - y);
+
+println!("la multiplicacion de {} y {} es {}", x, y, x * y);
+
+println!("la division de {} y {} es {}", x, y, x / y);
+
+println!("el modulo de {} y {} es {}", x, y, x % y);
+
+
+}
