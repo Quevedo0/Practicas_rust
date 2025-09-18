d84b769906974964f98ba50f95d95e4341aef1c7
Lissandro <luismexico2006@gmail.com>
2025-09-18 11:02:33 -0700

    Practica rust 1

---
+++ Practica1rustLuis.rs
@@ -0,0 +1,19 @@
+fn main() {
+ // Hecho por Luis Alejandro Aguilar Soberanes LICD
+
+    let suma = 5 + 5;
+    let resta = 5 - 4;
+    let division = 6 / 3;
+    let multiplicacion = 5 * 4;
+    let modulo = 10 % 3;
+    
+    println!("La suma es {}", suma);
+    println!("La resta es {}", resta);
+    println!("La división es {}", division);
+    println!("La multiplicación es {}", multiplicacion);
+    println!("El modulo es {}", modulo);
+    
+}
+
+
+
