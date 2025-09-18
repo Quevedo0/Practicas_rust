08b761697f9a74007c435dbd8a6ee40602847419
JorgeUrixl <jorgettorex333@gmail.com>
2025-09-18 11:11:25 -0700

    Jorge Uriel
    actividad 1 operaciones aritmeticas

---
+++ aritmetic.rs
@@ -0,0 +1,12 @@
+fn main(){
+let a = 30;
+let b = 13;
+let suma = a + b;
+let resta = a - b;
+let multi = a * b;
+let divi = a / b;
+println!("{}+{}={}",a,b,suma);
+println!("{}+{}={}",a,b,resta);
+println!("{}x{}={}",a,b,multi);
+println!("{}/{}={}",a,b,divi);
+}
