94314b03b5837c95551241619566e9c491352770
jissalvarod <gj.alvarez24@info.uas.edu.mx>
2025-09-18 11:00:52 -0700

    Add files via upload

---
+++ act2.rs
@@ -0,0 +1,17 @@
+fn main(){
+let g : i32 = 5;
+let j : i32 = 10;
+
+let _suma = g + j;	
+println!("suma= {}",_suma);
+
+let _resta = g - j;
+println!("resta= {}",_resta);
+
+let _multiplicacion = g * j;
+println!("multiplicacion= {}",_multiplicacion);
+
+let _residuo= g / j;
+println!("residuo= {}",_residuo);
+
+}
