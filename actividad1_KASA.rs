b7c5b227c4dd1563e6f2a2db8b095d33d626419a
kevinslas <kevinalfredosalas.fic@uas.edu.mx>
2025-09-18 11:01:56 -0700

    Add files via upload

---
+++ actividad1_KASA.rs
@@ -0,0 +1,16 @@
+fn main(){
+
+    let a = 2;
+    let b = 5;
+    
+    println!("Las variables son: {a} y {b}");
+    println!("el resultado de la suma de ambas variables es {}", a + b);
+    println!("el resultado de la resta de ambas variables es {}", a - b);
+    println!("el resultado de la multiplicacion de ambas variables es {}", a * b);
+    println!("el resultado de la division de ambas variables es {}", a * b);
+    
+    }
+
+    //BYKASA
+    /   \
+    |   |
