d2be41b77807260a484188f48944c788b1a93c06
Dayann306 <Wd.calderon24@info.uas.edu.mx>
2025-09-18 10:50:04 -0700

    Add files via upload

---
+++ tr.sh
@@ -0,0 +1,18 @@
+fn main (){
+
+let num1=8;
+let num2=9;
+
+let suma = num1 + num2;
+println!("La suma de {}",suma);
+
+let resta = num1 - num2;
+println!("La resta de {}",resta);
+
+let multiplicasion = num1 * num1;
+println!("La multiplicasion de {}",multiplicasion );
+
+let division = num1 / num2;
+println!("La division de {}",division);
+
+}
