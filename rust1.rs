ab47b9dfba84ec4506d9908cb0ac01d56bbbdc9b
omargerardofuentesh <omargerardofuentes.fic@uas.edu.mx>
2025-09-18 10:57:50 -0700

    Add files via upload

---
+++ rust1.rs
@@ -0,0 +1,18 @@
+fn main(){
+
+let a=10;
+let b=15;
+let suma=a+b;
+let resta=a-b;
+let multi=a*b;
+let divi=a/b;
+let resi=a%b;
+
+println!("la suma es:{}",suma);
+println!("la resta es:{}",resta);
+println!("la multiplicacion es:{}",multi);
+println!("la division es:{}",divi);
+println!("el residuo es:{}",resi);
+
+
+}
