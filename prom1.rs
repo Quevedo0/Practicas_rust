b3f70ae374d4857673c7c51ad63cb020b2860c73
Daniel Enrique Escobar <162261722+Tacasio@users.noreply.github.com>
2025-09-18 10:58:12 -0700

    Add files via upload

---
+++ prom1.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let num1 = 10;
+    let num2 = 20;
+
+    println!("Variables: num1 = {}, num2 = {}", num1, num2);
+
+    let suma = num1 + num2;
+    let resta = num1 - num2;
+    let multiplicacion = num1 * num2;
+    let division = num1 / num2;
+    let residuo = num1 % num2;
+
+    println!("\n--- Resultados ---");
+    println!("Suma: {}", suma);
+    println!("Resta: {}", resta);
+    println!("Multiplicación: {}", multiplicacion);
+    println!("División: {}", division);
+    println!("Residuo: {}", residuo);
+}
+
