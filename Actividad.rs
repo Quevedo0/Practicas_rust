c23f93a984d56c7060651273f2fe8fe4b5ac875a
Fabian Leonardo Loaiza Ramirez <fabianloaramirez1@gmail.com>
2025-09-18 11:01:44 -0700

    Actividad 1
    Loaiza Ramirez Fabian Leonardo

---
+++ Actividad.rs
@@ -0,0 +1,16 @@
+fn main(){
+
+let a = 10;
+let b = 5;
+
+println!("la suma de {a} y {b} es: {}", a + b);
+
+println!("la resta de {a} y {b} es: {}", a - b);
+
+println!("la multiplicacion de {a} y {b} es: {}", a * b);
+
+println!("la divicion de {a} y {b} es: {}", a / b);
+
+println!("el modulo de {a} y {b} es: {}", a % b);
+
+}
