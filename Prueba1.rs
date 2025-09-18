478ae2295008a76d07adc523c5dc93b45e5317b7
Diegotrevino <ds.trevino24@info.uas.edu.mx>
2025-09-18 11:00:04 -0700

    Ejercicio 1 DIEGO SANTOS TREVIÑO CAMACHO

---
+++ Prueba1.rs
@@ -0,0 +1,17 @@
+fn main(){
+
+//DIEGO SANTOS TREVIÑO CAMACHO
+
+let suma = 10 + 20;
+let resta = 30 - 7;
+let multiplicacion = 50 * 4;
+let division = 10 / 5;
+let modulo = 10 % 3;
+
+println!("La suma es: {}", suma);
+println!("La resta es: {}", resta);
+println!("La multiplicacion es: {}", multiplicacion);
+println!("La division es: {}", division);
+println!("El modulo es: {}", modulo);
+
+}
