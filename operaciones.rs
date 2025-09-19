76a748ebcca8ec520a70dcd99bb53277515745bc
Raul_eduardo <sotochipresraul@gmail.com>
2025-09-19 13:13:48 -0700

    Add files via upload

---
+++ operaciones.rs
@@ -0,0 +1,17 @@
+fn main()
+{
+let m= 5;
+let n= 2;
+let mut res;
+println!("Los numeros son:{} , {}",m,n);
+res=m+n;
+println!("suma:{}",res);
+res=m-n;
+println!("Resta:{}",res);
+res=m*n;
+println!("Producto:{}",res);
+res=m/n;
+println!("Division:{}",res);
+res=m%n;
+println!("Modulo:{}",res);
+}
