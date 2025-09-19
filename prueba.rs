6a5bc55d6aa3b6d46dc194dfc8e3ac5ff40143c3
Jesus Noe <jn.tapia21@info.uas.edu.mx>
2025-09-19 09:06:35 -0700

    CTP-Practica-Rust

---
+++ prueba.rs
@@ -0,0 +1,17 @@
+fn main(){
+
+let num1=15;	
+let num2=2;
+	let suma = num1 + num2;
+	let resta = num1 - num2;
+	let multiplicacion = num1 * num2;
+	let division = num1 / num2;
+	let resto = num1 % num2;
+
+println!("Los valores son num1=15 y num2=2");
+println!("Suma:{}", suma);
+println!("Resta:{}", resta);
+println!("Multiplicacion:{}", multiplicacion);
+println!("Division:{}", division);
+println!("Resto:{}", resto);
+}
